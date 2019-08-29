367ef17fc60cb5153afebef4bd1edc9b389f0f4d
matteobrogin <mattesackboy@gmail.com>
2019-08-29 11:40:11 +0200

    Disabled hammerjs to enable pinch to zoom on mobile devices

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import 'hammerjs';
+//import 'hammerjs'; Importing it will disable pinch to zoom browser behavior on images, but will enable changing image swiping
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
