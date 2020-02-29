e612c46e529db9960b0da0238a008d89c21c691f
dcostan <davide.cst00@gmail.com>
2020-02-29 14:45:32 +0100

    Updated script

--- number.py
+++ number.py
@@ -3,6 +3,10 @@ from PIL import ImageFont
 from PIL import ImageDraw
 from glob import glob
 from os import mkdir, remove
+from distutils.dir_util import copy_tree
+
+def weightPercentage(img_size_1, img_size_2, percentage):
+    return int(max(img_size_1, img_size_2) * percentage / 100)
 
 try:
     mkdir("images")
@@ -14,12 +18,15 @@ for folder in glob("images_raw/*"):
     try:
         mkdir(folder.replace("images_raw", "images", 1))
     except:
-        for f in glob(folder.replace("images_raw", "images", 1) + "/*"):
+        for f in glob(folder.replace("images_raw", "images", 1) + "/*.*"):
             remove(f)
-    for file in glob(folder + "/*"):
+    for file in sorted(glob(folder + "/*.*")):
         img = Image.open(file)
         draw = ImageDraw.Draw(img)
-        font = ImageFont.truetype("arial.ttf", 25)
-        draw.text((img.size[0] - 50, img.size[1] - 40), str(counter), (255,255,255),font=font)
+        font = ImageFont.truetype("arial.ttf", weightPercentage(img.size[0], img.size[1], 3))
+        draw.text((img.size[0] - weightPercentage(img.size[0], img.size[1], 6), img.size[1] - weightPercentage(img.size[0], img.size[1], 5)),
+                 str(counter), (255,255,255),font=font)
         img.save(file.replace("images_raw", "images", 1))
         counter = counter + 1
+
+copy_tree("images_raw/brands", "images/brands")
